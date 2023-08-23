6b1bcff01df9dc264f0f0af30c7290fb30ed6bc4
KRShee <KRShee@gmail.com>
2023-08-22 20:55:34 -0500

    filenameChanges

---
+++ Lab1/Class_list.py
@@ -0,0 +1,6 @@
+'''Part 2: List of classes
+
+Write a program that asks for the names of all of the classes you are taking this semester
+Save these class names in a list.
+Print all the items in the list, one per line.'''
+
